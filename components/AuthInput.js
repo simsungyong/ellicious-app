0f000d63c0f105a908a8de88142fd0140e1dbcc2
simsungyong <31848384+simsungyong@users.noreply.github.com>
2019-11-04 16:27:17 +0900

    Update AuthInput.js

--- components/AuthInput.js
+++ components/AuthInput.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 import PropTypes from "prop-types";
 import constants from "../constants";
 import {PointPink, TINT_COLOR} from './Color'
-
+/*
 class FloatingLabelInput extends Component {
   state = {
     isFocused: false,
@@ -84,7 +84,7 @@ class AuthInputClass extends Component {
     );
   }
 }
-
+*/
 const Container = styled.View`
   margin-bottom: 10px;
 `;

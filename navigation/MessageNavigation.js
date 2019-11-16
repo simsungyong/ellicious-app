af3ffb313d4ee5c99938022fc1f8c361542f86de
hongjoonki <ghdjk2216@naver.com>
2019-11-16 14:27:41 +0900

    Messages

--- navigation/MessageNavigation.js
+++ navigation/MessageNavigation.js
@@ -1,34 +1,22 @@
 import React from "react";
-import { TextInput, TouchableOpacity } from "react-native";
 import { createStackNavigator } from "react-navigation-stack";
 import Messages from "../screens/Messages/Messages";
 import MessageRoom from "../screens/Messages/MessageRoom";
-import Message from "../screens/Messages/Message";
-import constants from "../constants";
-import styles from "../styles";
-import { LightGrey } from "../components/Color";
+import MessageDetail from "../screens/Messages/MessageDetail";
+
 
 export default createStackNavigator({
   Messages: {
     screen: Messages,
     navigationOptions: {
-      headerTitle: (
-        <TextInput
-          style={{
-              width: constants.width - 40,
-              height: 35,
-              backgroundColor: LightGrey,
-              padding: 10,
-              borderRadius: 5,
-              textAlign: "center"
-          }}
-          returnKeyType="search"
-          placeholder={"Search"}
-          placeholderTextColor={styles.darkGreyColor}
-        />
-      )
+      headerTitle: "Message"
     }
   },
-  Message,
+  MessageDetail: {
+    screen: MessageDetail,
+    navigationOptions: ({ navigation }) => ({
+      title: navigation.getParam("username")
+    })
+  },
   MessageRoom
 });

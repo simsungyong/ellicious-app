52962ff8feda4b125ad450e8320f1abc39c40d3b
hongjoonki <ghdjk2216@naver.com>
2020-01-08 22:18:55 +0900

    코믿

--- screens/UserDetail.js
+++ screens/UserDetail.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "react-apollo-hooks";
 import { gql } from "apollo-boost";
 import { USER_FRAGMENT } from "../fragments";
 import Loader from "../components/Loader";
-import { ScrollView, Text } from "react-native";
+import { ScrollView, Text, RefreshControl } from "react-native";
 import UserProfile from "../components/UserProfile";
 
 export const GET_USER = gql`
@@ -16,12 +16,29 @@ export const GET_USER = gql`
 `;
 
 export default ({ navigation }) => {
-  const { loading, data } = useQuery(GET_USER, {
+  const [refreshing, setRefreshing] = useState(false);
+
+  const { loading, data, refetch } = useQuery(GET_USER, {
     variables: { id: navigation.getParam("id") }
   });
+
+  const refresh = async() =>{
+    try{
+      setRefreshing(true);
+      await refetch();
+      
+    }catch (e){
+      console.log(e);
+    }finally{
+      setRefreshing(false);
+    }
+  };
   
   return (
-    <ScrollView>
+      <ScrollView
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={refresh}/>
+        }>
       {loading ? (
         <Loader />
       ) : (

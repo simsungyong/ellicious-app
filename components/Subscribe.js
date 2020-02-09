56b4aa343575c391807cdbb9ffdb834d923067cd
hongjoonki <ghdjk2216@naver.com>
2020-02-10 00:46:40 +0900

    commit

--- components/Subscribe.js
+++ components/Subscribe.js
@@ -4,30 +4,15 @@ import PropTypes from "prop-types";
 import { useQuery, useSubscription, useMutation } from "react-apollo-hooks";
 import gql from "graphql-tag";
 import MainNavigation from "../navigation/MainNavigation";
-import {MESSAGES, MessageDetailPresenter} from "../screens/Messages/MessageDetail/MessageDetailPresenter";
-import {SEE_ROOMS} from "../screens/Messages/Messages"
 
 const ME = gql`
     {
         me {
             id
+            username
+            avatar
         }
     }
-`;
-
-const NEW_MESSAGE = gql`
-  subscription newMessage($userId: String) {
-    newMessage(userId: $userId) {
-      id
-      text
-      from {
-        username
-      }
-      room {
-        id
-      }
-    }
-  }
 `;
 
 
@@ -37,47 +22,11 @@ const Subscribe = () => {
   const [messageOK, setMessageOK] = useState(false)
 
   const { data } = useQuery(ME);
-  if(data && userId=="") {
-      setUserId(data.me.id);
+  if (data && userId == "") {
+    setUserId(data.me.id);
   }
 
-  const { data: newMessage } = useSubscription(NEW_MESSAGE, {
-    variables: {
-      userId: userId
-    }, suspend: true
-  });
-  const { data: data2, refetch:refetch2 } = useQuery(SEE_ROOMS);
-  
-  
-  const update = async() => {
-    if(userId!=="" && newMessage !== undefined) {
-      try {
-        await refetch2();
-        const { data: data3,  refetch:refetch3 } = useQuery(
-          MESSAGES, {
-            variables: {
-              roomId: newMessage.newMessage.room.id
-            }
-          }
-        ).then(refetch3);
-      } catch (e) {
-        console.log(e)
-      } finally {
-        console.log("hello!")
-      }
-    }
-  }
-  
-  
-
-  useEffect(() => {
-    update();
-  }, [newMessage])
-  
-    
-
-
-    return <MainNavigation />
+  return <MainNavigation />
 }
 
 export default Subscribe;

fa7dea1baf49e0fbeec1126a63fa39b783d19dc3
Sim sungyong <ssy332972@gmail.com>
2020-02-10 17:13:00 +0900

    f

--- screens/Messages/MessageDetail/ChatScreen.js
+++ screens/Messages/MessageDetail/ChatScreen.js
@@ -49,33 +49,31 @@ export default class ChatScreen extends React.Component {
 
     getlast(){
         let a = firebase.database().ref('messages').child(User.userId).child(this.state.person.userId).limitToLast(1);
-        a.on("value",(value)=>{
-            console.log(value.message)
-            console.log(value.val())
+        a.on("child_added", (value)=>{
+            console.log(value.val().message)
         })
-        
     }
 
     sendMessage = async () => {
-        this.getlast()
-        // if(this.state.textMessage.length > 0){
-        //     let msgId = firebase.database().ref('messages').child(User.userId).child(this.state.person.userId).push().key;
-        //     let updates={};
-        //     let message={
-        //         message:this.state.textMessage,
-        //         time:firebase.database.ServerValue.TIMESTAMP,
-        //         from: User.username
-        //     }
-        //     firebase.database().ref('users/'+User.userId+'/friends/'+this.state.person.userId).set({ID: this.state.person.username, userId: this.state.person.userId});
-        //     firebase.database().ref('users/'+this.state.person.userId+'/friends/'+User.userId).set({ID: User.username, userId: User.userId});
+        let msgId = firebase.database().ref('messages').child(User.userId).child(this.state.person.userId).push().key;
+        if(this.state.textMessage.length > 0){
+            let msgId = firebase.database().ref('messages').child(User.userId).child(this.state.person.userId).push().key;
+            let updates={};
+            let message={
+                message:this.state.textMessage,
+                time:firebase.database.ServerValue.TIMESTAMP,
+                from: User.username
+            }
+            firebase.database().ref('users/'+User.userId+'/friends/'+this.state.person.userId).set({ID: this.state.person.username, userId: this.state.person.userId});
+            firebase.database().ref('users/'+this.state.person.userId+'/friends/'+User.userId).set({ID: User.username, userId: User.userId});
 
             
 
-        //     updates['messages/'+User.userId+'/'+this.state.person.userId+'/'+msgId] = message;
-        //     updates['messages/'+this.state.person.userId+'/'+User.userId+'/'+msgId] = message;
-        //     firebase.database().ref().update(updates);
-        //     this.setState({textMessage:''})
-        //}
+            updates['messages/'+User.userId+'/'+this.state.person.userId+'/'+msgId] = message;
+            updates['messages/'+this.state.person.userId+'/'+User.userId+'/'+msgId] = message;
+            firebase.database().ref().update(updates);
+            this.setState({textMessage:''})
+        }
     }
 
 

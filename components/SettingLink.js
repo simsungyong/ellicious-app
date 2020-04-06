b44b6cb7c45885616c7ebe2696038cc79656c44d
hongjoonki <ghdjk2216@naver.com>
2020-04-06 20:32:05 +0900

    c

--- components/SettingLink.js
+++ components/SettingLink.js
@@ -1,17 +1,16 @@
 import React, { useState } from "react";
-import { Alert, StyleSheet, AsyncStorage } from "react-native";
+import { StyleSheet, Modal, View, TouchableOpacity } from "react-native";
 import styled from "styled-components";
 import { withNavigation } from "react-navigation";
 import { MaterialCommunityIcons } from "@expo/vector-icons"
-import { PointPink, mainPink, TINT_COLOR, LightGrey } from "./Color";
-import Modal, { ModalTitle, ModalContent, ModalFooter, ModalButton } from 'react-native-modals';
+import { mainPink, TINT_COLOR, LightGrey } from "./Color";
 import { useLogOut } from "../AuthContext";
 
 const Container = styled.TouchableOpacity``;
 const Text = styled.Text`
     
 `;
-const View = styled.View`
+const ViewO = styled.View`
   margin-left : 10px;
   margin-right : 8px;
   margin-top : 1px;
@@ -33,7 +32,7 @@ const SettingLink = ({ navigation }) => {
     return (
         <>
             <Container onPress={() => setIsModalPick(true)}>
-                <View>
+                <ViewO>
                     <BG style={styles.CircleShapeView}>
                         <MaterialCommunityIcons
                             color={TINT_COLOR}
@@ -41,24 +40,63 @@ const SettingLink = ({ navigation }) => {
                             name={"logout"}
                         />
                     </BG>
-                </View>
+                </ViewO>
             </Container>
+            
             <Modal
-                visible={isModalPick}
-                onTouchOutside={() => setIsModalPick(false)}
-                width={0.8}
-                onSwipeOut={() => setIsModalPick(false)}
+            visible={isModalPick}
             >
-                <ModalContent>
-                    <Text>로그아웃 하시겠습니까?</Text>
-                </ModalContent>
+            <View
+                    style={{
+                        flex: 1,
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        borderRadius: 20,
+                        backgroundColor: 'rgba(0,0,0,0.50)'
+                    }}
+                >
+                    <View style={{
+                        width: 300,
+                        height: 150,
+                        backgroundColor: 'white',
+                        borderRadius: 20,
+                        
+                    }}>
 
-                <ModalFooter>
-                    <ModalButton onPress={() => logout()} text="확인" />
+                    
+                        <Text
+                            style={{ fontSize: 16, alignSelf: 'center', marginTop: 40, flex: 7, alignItems:'center', justifyContent: 'center'}}
+                        >
+                            {"로그아웃 하시겠습니까?"}
+                        </Text>
+                        <View
+                            style={{
+                                alignSelf: 'baseline',
+                                backgroundColor: mainPink,
+                                width: 300,
+                                flex: 4,
+                                borderBottomLeftRadius: 20,
+                                borderBottomRightRadius: 20,
+                                flexDirection: 'row'
+                            }}
+                        >
+                            <TouchableOpacity
+                                style={{ flex: 1, justifyContent: 'center', alignItems: 'center', }}
+                                onPress={() => {logout()}}>
+                                    <Text style={{ color: 'white', fontSize: 15 }}>확인</Text>
+                            </TouchableOpacity>
 
-                    <ModalButton onPress={() => setIsModalPick(false)} text="취소" />
+                            <TouchableOpacity
+                                style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+                                onPress={() => setIsModalPick(false)}>
+                                <Text style={{ color: 'white', fontSize: 15 }}>취소</Text>
+                            </TouchableOpacity>
 
-                </ModalFooter>
+                        </View>
+                    </View>
+
+                </View>
             </Modal>
         </>
     )
